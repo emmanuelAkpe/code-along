89de62666c8a83d71dd259bddb3c1359d00d78ca
Emmanuel Akpe <emmanuelakpe1@gmail.com>
2022-03-23 13:52:27 +0000

    react components 3 week 3 day 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import ProfileCard from "./ProfileCard";
-import writers from "./writers";
+
 
 function App() {
+
+  const [writers, setWriters]=useState([])
+
+  useEffect(()=>{
+      const getWriters = async ()=>{
+      const response = await fetch('/writers.json')
+      const data = await response.json()
+      setWriters(data)
+
+    }
+
+    getWriters()
+  }, [])
   return (
     <div className="App">
       <h1>Writer Profiles.</h1>
       <div className="container">
         {writers.map((writer) => (
 
-          <ProfileCard writer={writer}/>
+          <ProfileCard writer={writer} key={writer.id}/>
 
         ))}
       </div>
